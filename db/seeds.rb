2fd30f5f1f34efca1ef9904f5c0058a0eb1fe380
andrevieira11 <andre.pa.vieira@hotmail.com>
2021-01-04 23:25:44 +0000

    Add files via upload

---
+++ db/seeds.rb
@@ -0,0 +1,46 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# Create a main sample user.
+User.create!(name: "Example User",
+                email: "example@railstutorial.org", 
+                phone_number: 912345678, 
+                street: "Street", 
+                zip_code: 9472, 
+                city: "LA", 
+                country: "USA", 
+                password: "foobar", 
+                password_confirmation: "foobar",
+                 admin: true)
+
+# Generate a bunch of additional users.
+99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    phone_number = 912345678
+    street = "Street"
+    zip_code = 947#n
+    city = "LA"
+    country = "USA"
+    password = "password"
+    User.create!(name: name, email: email, phone_number: phone_number, street: street, zip_code: zip_code, city: city, country: country, password: password, password_confirmation: password)
+end
+
+# Generate microposts for a subset of users.
+users = User.order(:created_at).take(6)
+50.times do
+    content = Faker::Lorem.sentence(word_count: 5)
+    users.each { |user| user.microposts.create!(content: content) }
+end
+
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
